aee80d0e6ccb32a63d4993e47d37e7b6e3279ed6
Diviricean Gabriel <q3hannibal@gmail.com>
2022-12-04 16:52:35 +0200

    add day 1

---
+++ day1/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func sum(array []int) int {
+	total := 0
+
+	for _, n := range array {
+		total += n
+	}
+
+	return total
+}
+
+func main() {
+	fmt.Println("Advent of code")
+
+	input, err := os.ReadFile("input")
+	check(err)
+
+	inputArray := strings.Split(string(input), "\r\n\r\n")
+
+	highestCalories := 0
+	allCalories := []int{}
+
+	for _, s := range inputArray {
+		caloriesArray := []int{}
+		splitArray := strings.Split(s, "\r\n")
+
+		for _, s := range splitArray {
+			n, err := strconv.Atoi(s)
+			check(err)
+
+			caloriesArray = append(caloriesArray, n)
+		}
+		sum := sum(caloriesArray)
+
+		if sum > highestCalories {
+			highestCalories = sum
+		}
+
+		allCalories = append(allCalories, sum)
+
+	}
+
+	sort.Ints(allCalories)
+
+	biggestThree := allCalories[len(allCalories)-3:]
+	totalThree := sum(biggestThree)
+
+	fmt.Println(highestCalories)
+	fmt.Println(totalThree)
+
+}
